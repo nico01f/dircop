fe9830db78fdc74412db6813532fc5d7185a2c76
Nico Fuenzalida <nico01f@gmail.com>
2016-08-11 11:53:08 -0400

    agrega directorios

--- compara.rb
+++ compara.rb
@@ -1,7 +1,14 @@
 require 'yaml'
-
-# A
+require ''
+# Arreglo de conexion
 CONEXION = YAML.load_file('servers.yaml') unless defined? CONEXION
+#Listado de directorios a buscar
+DIRECTORIOS = YAML.load_file('directorios.yaml') unless defined? DIRECTORIOS
 
 serv = CONEXION['web01']
 puerto= CONEXION['puerto'].to_s
+
+
+DIRECTORIOS.each { |dir|
+  # Comprueba si existe directorio
+  }
