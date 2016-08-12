3a9d19610f23e387c354a7c2556c01d073963d9b
Nico Fuenzalida <nico01f@gmail.com>
2016-08-12 12:38:52 -0400

    mixlib, change to scripts, created clases

--- compara.rb
+++ compara.rb
@@ -3,31 +3,54 @@
 require 'yaml'
 require 'colorize'
 require 'net/ssh'
+require 'mixlib/cli'
 
-# Servers arrays
-CONN = YAML.load_file('servers.yaml') unless defined? CONN
+class ComparaDirOptions
+  include Mixlib::CLI
 
-# Directories arrays
-DIR_LIST = YAML.load_file('directorios.yaml') unless defined? DIR_LIST
+  option :servers,
+    :short        => "-s /path/to/servers.yaml",
+    :long         => "--servers /path/to/servers.yaml",
+    :description  => "yaml file with servers",
+    :default      => "servers.yaml"
 
-# TO-DO: Loop servers
-path = '/opt/autentia/www'
-default_user = 'root'
-default_port = '14225'
+  option :directories,
+    :short        => "-d /path/to/directories.yaml",
+    :long         => "--directories /path/to/directories.yaml",
+    :description  => "yaml file with directories to compare",
+    :default      => "directories.yaml"
+end
+#path = '/opt/autentia/www'
+#default_user = 'root'
+#default_port = '14225'
 
-CONN.each do |web,srv|
-  Net::SSH.start(srv, default_user, :port => default_port) do |ssh|
-    DIR_LIST.each { |dir|
-      output = ssh.exec!("[ -d #{path}/#{dir} ] && OK")
-      if output.include? "OK"
-        # Verbose mode
-        print "[#{web}] [INFO] - ".green
-        puts "Directorio coincide"
-      else
-        # Verbose mode
-        print "[#{web}] [WARN] - ".red
-        puts "Revisar directorio en: #{path}/#{dir}\n"
-      end
-    }
+options = ComparaDirOptions.new
+options.parse_options
+
+def run(options)
+  @servers.each do |web,srv|
+    Net::SSH.start(srv, options[:default_user], :port => options[:default_port]) do |ssh|
+      dirs.each { |dir|
+        output = ssh.exec!("[ -d #{path}/#{dir} ] && OK")
+        if output.include? "OK"
+          # Verbose mode
+          print "[#{web}] [INFO] - ".green
+          puts "Directorio coincide"
+        else
+          # Verbose mode
+          print "[#{web}] [WARN] - ".red
+          puts "Revisar directorio en: #{path}/#{dir}\n"
+        end
+      }
+    end
   end
 end
+
+private
+  def servers
+    @servers ||= YAML.load_file(options.config[:servers])
+  end
+
+  def dirs
+    @dirs ||= YAML.load_files(options.config[:directories])
+  end
